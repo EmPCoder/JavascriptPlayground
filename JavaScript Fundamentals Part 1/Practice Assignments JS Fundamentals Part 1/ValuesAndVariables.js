1463add246ccddfbd4919dd412a60b2e75b4830b
Matt Hudock <matthewd1715@gmail.com>
2022-06-17 14:37:21 -0400

    Completing Values and Vars Practice Assignment

---
+++ JavaScript Fundamentals Part 1/Practice Assignments JS Fundamentals Part 1/ValuesAndVariables.js
@@ -0,0 +1,13 @@
+/* 1. Declare variables called 'country', 'continent' and 'population' and 
+assign their values according to your own country (population in millions)
+2. Log their values to the console */
+
+let country = 'United States of America';
+let continent = 'North America';
+let population = 329500000;
+
+let date = new Date();
+
+console.log(date);
+
+console.log('I am from the ' + country + ' which is on the contentinent ' + continent + ' which currently has a population of ' + population);
