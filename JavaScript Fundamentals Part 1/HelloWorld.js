6ad715c24b0a31eee2348b87757fd7ef23085811
Matt Hudock <matthewd1715@gmail.com>
2022-06-17 13:59:57 -0400

    Removing Folder + Adding Header

--- JavaScript Fundamentals Part 1/HelloWorld.js
+++ JavaScript Fundamentals Part 1/HelloWorld.js
@@ -1,13 +1,20 @@
+// alert Hello World
 alert("Hello World");
 
+// Assigning var js to = amazing
 let js = 'amazing';
 
+// If js == amazing alert the below
 if (js === 'amazing') alert('JavaScript is FUN!');
 
+// Changes js to = boring
 js = 'boring'
 
+// If Js = boring alert the below
 if (js === 'boring') alert('JavaScript is BORING');
 
+// Basic Math Operation
 40 + 8 + 25;
 
+// Logs text to the console
 console.log('Logging text to console');
