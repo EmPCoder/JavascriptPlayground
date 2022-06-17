beb5fd75103b2aca6b9f137b1819b82ef01dfec2
Matt Hudock <matthewd1715@gmail.com>
2022-06-17 13:44:09 -0400

    Committing first hello world

---
+++ JavaScript Fundamentals Part 1/Hello World/HelloWorld.js
@@ -0,0 +1,13 @@
+alert("Hello World");
+
+let js = 'amazing';
+
+if (js === 'amazing') alert('JavaScript is FUN!');
+
+js = 'boring'
+
+if (js === 'boring') alert('JavaScript is BORING');
+
+40 + 8 + 25;
+
+console.log('Logging text to console');
