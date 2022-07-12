1956cb9270ceda28b9ad22c252a05844a86eefb0
Matt Hudock <matthewd1715@gmail.com>
2022-07-12 09:19:51 -0400

    Adding Operator Precedence Lecture

---
+++ JavaScript Fundamentals Part 1/Lectures/OperatorPrecedence.js
@@ -0,0 +1,14 @@
+const now = 2037;
+const ageMatt = now - 1995;
+const ageLindsey = now - 1999;
+
+console.log(now - 1995 > now - 1999);
+
+console.log(25 - 10 - 5); // = 10
+
+let x, y; // declaring 2 vars
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const avgAge = (ageMatt + ageLindsey) / 2;
+console.log(ageMatt, ageLindsey, avgAge);
