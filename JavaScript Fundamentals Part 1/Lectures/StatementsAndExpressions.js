65fc0313ef272b6212a4404ec3a0c31f2d9311dc
Matt <matthewd1715@gmail.com>
2022-11-18 08:25:05 -0800

    Statements and Expressions Lecture

---
+++ JavaScript Fundamentals Part 1/Lectures/StatementsAndExpressions.js
@@ -0,0 +1,12 @@
+//Expression = Piece of code that produces a value
+3 + 4;
+1995;
+true && false && !false;
+
+//Statements Doesn't necissarily produce a value.
+if (23 > 10) {
+    const str = '23 is bigger'; //String itself is a expression
+}
+
+const me = 'Matt';
+console.log(`I'm ${2037 - 1995} years old ${me}`);
