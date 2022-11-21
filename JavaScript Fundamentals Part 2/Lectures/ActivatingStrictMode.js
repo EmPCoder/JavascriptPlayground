972afc8a414c54b20553418e7148502b780fb31b
Matt <matthewd1715@gmail.com>
2022-11-21 13:38:54 -0500

    Fixing Typo in Folder + Adding Activating Strict Mode Lecture

---
+++ JavaScript Fundamentals Part 2/Lectures/ActivatingStrictMode.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true; 
+if (hasDriversLicense) console.log(`I can drive 😄`);
+
+//Keyword error by using interface
+// const interface = 'Audio';
+
+//Keyword error by using private
+// const private = 543;
