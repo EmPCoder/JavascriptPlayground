f9c55f1084bdda2396bd42e3e5f8b3dc2ab24ca8
Matt <matthewd1715@gmail.com>
2023-03-16 12:46:35 -0400

    Committing IterationForLoop Lecture + Practice

---
+++ JavaScript Fundamentals Part 2/Lectures/IterationTheForLoop.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// console.log(`Lifting weights repition 1 🏋`); 
+// console.log(`Lifting weights repition 2 🏋`); 
+// console.log(`Lifting weights repition 3 🏋`); 
+// console.log(`Lifting weights repition 4 🏋`); 
+// console.log(`Lifting weights repition 5 🏋`); 
+// console.log(`Lifting weights repition 6 🏋`); 
+// console.log(`Lifting weights repition 7 🏋`); 
+// console.log(`Lifting weights repition 8 🏋`); 
+// console.log(`Lifting weights repition 9 🏋`); 
+// console.log(`Lifting weights repition 10 🏋`); 
+
+//For loop keeps running while condition is TRUE.
+//For loop that defines var rep, if rep <= 30 will print below clg statemnt
+for(let rep = 1; rep <= 30; rep++) {
+    console.log(`Lifting weights repition ${rep} 🏋`); 
+}
+
---
+++ JavaScript Fundamentals Part 2/Practice Assignments JS Fundamentals Part 2/IterationTheForLoop.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*
+There are elections in your country! In a small town, there are only 50 voters. 
+Use a for loop to simulate the 50 people voting, by logging a string like this to 
+the console (for numbers 1 to 50): 'Voter number 1 is currently voting'
+*/
+
+//For loop that sets voters = 1, as long as voters <= 50 will print out clg line
+for(let voters = 1; voters <= 50; voters++){
+    console.log(`Voter number ${voters} is currently voting 🗳 `); 
+}
