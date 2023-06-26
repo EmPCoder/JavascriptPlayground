8674d5099a0f1cfe36c7ee919baf9256c7425d83
Matthew Hudock <matthewd1715@gmail.com>
2023-06-26 12:10:50 -0400

    Demonstrated how to manipulate other classes in JS

--- JavaScript in the Browser DOM and Events Fundamentals/Project #1 Guess My Number/script.js
+++ JavaScript in the Browser DOM and Events Fundamentals/Project #1 Guess My Number/script.js
@@ -2,3 +2,18 @@
 
 //Same way we would select using CSS #for ID etc...
 console.log(document.querySelector('.message').textContent);
+
+//Changes the message class from start guessing... to 🎉Correct Number!
+document.querySelector('.message').textContent = '🎉 Correct Number!' 
+
+//manipulates the number block to = 13
+document.querySelector('.number').textContent = 13;
+
+//manipulates the score to = 10
+document.querySelector('.score').textContent = 10;
+
+//Adds 23 into the input field.
+document.querySelector('.guess').value = 23;
+
+//logs the input value of the guess class to the console.
+console.log(document.querySelector('.guess').value);
