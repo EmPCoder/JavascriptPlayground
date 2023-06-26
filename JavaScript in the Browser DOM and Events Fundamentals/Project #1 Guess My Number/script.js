3769f5c3d05e098d096d944ef1f322b76bb97fb0
Matthew Hudock <matthewd1715@gmail.com>
2023-06-26 12:58:55 -0400

    Implementing more logic around click events and what happens with them

--- JavaScript in the Browser DOM and Events Fundamentals/Project #1 Guess My Number/script.js
+++ JavaScript in the Browser DOM and Events Fundamentals/Project #1 Guess My Number/script.js
@@ -1,19 +1,34 @@
 'use strict';
 
-//Same way we would select using CSS #for ID etc...
-console.log(document.querySelector('.message').textContent);
+// //Same way we would select using CSS #for ID etc...
+// console.log(document.querySelector('.message').textContent);
 
-//Changes the message class from start guessing... to 🎉Correct Number!
-document.querySelector('.message').textContent = '🎉 Correct Number!' 
+// //Changes the message class from start guessing... to 🎉Correct Number!
+// document.querySelector('.message').textContent = '🎉 Correct Number!' 
 
-//manipulates the number block to = 13
-document.querySelector('.number').textContent = 13;
+// //manipulates the number block to = 13
+// document.querySelector('.number').textContent = 13;
 
-//manipulates the score to = 10
-document.querySelector('.score').textContent = 10;
+// //manipulates the score to = 10
+// document.querySelector('.score').textContent = 10;
 
-//Adds 23 into the input field.
-document.querySelector('.guess').value = 23;
+// //Adds 23 into the input field.
+// document.querySelector('.guess').value = 23;
 
-//logs the input value of the guess class to the console.
-console.log(document.querySelector('.guess').value);
+// //logs the input value of the guess class to the console.
+// console.log(document.querySelector('.guess').value);
+
+// selects the class .check, adds a function that expects and input from the box above the check button
+// outputs whatever number was input into that box to the console.
+document.querySelector('.check').addEventListener('click', function() {
+    // console.log(document.querySelector('.guess').value);
+
+    //Creates var guess and assigns the value = to whatever number is input in the box above check!
+    const guess = Number(document.querySelector('.guess').value);
+
+    console.log(guess, typeof guess);
+
+    if(!guess) {
+        document.querySelector('.message').textContent = '⛔ No number!'
+    }
+});
