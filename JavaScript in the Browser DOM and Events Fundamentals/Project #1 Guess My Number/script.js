83caa87d6e7a8e82f92a704cdcae573872c0615d
Matthew Hudock <matthewd1715@gmail.com>
2023-07-24 14:24:23 -0400

    Implementing first part of game logic

--- JavaScript in the Browser DOM and Events Fundamentals/Project #1 Guess My Number/script.js
+++ JavaScript in the Browser DOM and Events Fundamentals/Project #1 Guess My Number/script.js
@@ -18,6 +18,16 @@
 // //logs the input value of the guess class to the console.
 // console.log(document.querySelector('.guess').value);
 
+// Defines variable number, and assigns it = random num between 1 & 20
+// trunc is there for making it a whole number.
+const secretNumber = Math.trunc(Math.random()*20)+1
+
+// Creating variable score and setting = 20.
+let score = 20;
+
+// Selecting the number element, and displaying what the random number generated above is.
+document.querySelector('.number').textContent = secretNumber;
+
 // selects the class .check, adds a function that expects and input from the box above the check button
 // outputs whatever number was input into that box to the console.
 document.querySelector('.check').addEventListener('click', function() {
@@ -29,6 +39,34 @@ document.querySelector('.check').addEventListener('click', function() {
     console.log(guess, typeof guess);
 
     if(!guess) {
-        document.querySelector('.message').textContent = '⛔ No number!'
+        document.querySelector('.message').textContent = '⛔ No Number!'
+
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!' 
+
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = '📈 Too High!';
+            score = score - 1;
+            document.querySelector('.score').textContent = score;
+        }
+        else {
+            document.querySelector('.message').textContent = '💥 You lost the game, please try again.';
+            document.querySelector('.score').textContent = 0
+        }
+
+
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = '📉 Too Low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else {
+            document.querySelector('.message').textContent = '💥 You lost the game, please try again.';
+            document.querySelector('.score').textContent = 0;
+        }
+
     }
+    
 });
