bc0d8917777740fe6c834113a0febc6088a248b9
Matthew Hudock <matthewd1715@gmail.com>
2023-09-13 13:54:41 -0400

    Adding ability to press ESC to exit Modal

--- JavaScript in the Browser DOM and Events Fundamentals/Project #2 Modal Window/script.js
+++ JavaScript in the Browser DOM and Events Fundamentals/Project #2 Modal Window/script.js
@@ -8,9 +8,15 @@ const btnsShowModal = document.querySelectorAll('.show-modal');
 
 // Defining cont openModal = to the ability to open the modal.
 const openModal = function() {
-    console.log('Button Clicked');
+    // console.log('Button Clicked');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
+}
+
+// Defining cont closeModal = to the ability to close the open modal, either on x button or overlay.
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 }
 
 /* For loop that adds a click event listener to each object, also stating that when the btns are 
@@ -20,13 +26,17 @@ for (let i = 0; i < btnsShowModal.length; i++) {
     console.log(btnsShowModal[i].addEventListener('click', openModal));
 }
 
-// Defining cont closeModal = to the ability to close the open modal, either on x button or overlay.
-const closeModal = function(){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-}
-
 // Calling the function closeModal on both the Button and the Overlay.
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+// Adding event Listener to the entire document, listening for a key press, if = Escape Key close modal.
+document.addEventListener('keydown', function(e) {
+    // console.log(e.key);
+
+    // If statement, checking if the key press = Escape, and if modal doesn't contain hidden.
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModal();
+    }
+});
 
