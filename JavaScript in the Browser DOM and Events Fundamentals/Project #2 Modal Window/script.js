6d06454a489995b609a0765839f49c7ba49ebbdd
Matthew Hudock <matthewd1715@gmail.com>
2023-09-13 11:58:07 -0400

    Implementing Starter CSS and HTML file, assigning modal classes to variables and Removing Unneccesary folders/files from Source

--- JavaScript in the Browser DOM and Events Fundamentals/Project #2 Modal Window/script.js
+++ JavaScript in the Browser DOM and Events Fundamentals/Project #2 Modal Window/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+/* When using query selector with multiple elements of the same name it only grabs the first one */
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsShowModal);
+
+// For loop that logs the text content of each modal element
+for (let i = 0; i < btnsShowModal.length; i++) {
+    console.log(btnsShowModal[i].textContent);
+}
