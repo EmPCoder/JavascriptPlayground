8d9589b7abd71ec433f75bf22eb500c7ba755a4a
Matt <matthewd1715@gmail.com>
2023-11-28 12:32:20 -0500

    Setting starting scores = 0 + hiding dice by default

--- JavaScript in the Browser DOM and Events Fundamentals/Project #3 Pig Game/script.js
+++ JavaScript in the Browser DOM and Events Fundamentals/Project #3 Pig Game/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+//Selecting ID of score0
+const score0El = document.querySelector('#score--0');
+//Works same as above without #
+const score1El = document.getElementById('score--1');
+//Grabbing class dice from html
+const diceEl = document.querySelector('.dice');
+
+//Setting Values of score elements for starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
