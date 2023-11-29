3cff4e8da7aabbb45f4f61548512a0ee0bf1710f
Matt <matthewd1715@gmail.com>
2023-11-29 11:35:55 -0500

    Adding ability to reset score when player rolls a 1, while switching the active player background

--- JavaScript in the Browser DOM and Events Fundamentals/Project #3 Pig Game/script.js
+++ JavaScript in the Browser DOM and Events Fundamentals/Project #3 Pig Game/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 //Selecting ID of score0
 const score0El = document.querySelector('#score--0');
 //Works same as above without #
@@ -18,7 +20,9 @@ score1El.textContent = 0;
 //Hiding dice by default, by adding hidden class.
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function() {
@@ -35,11 +39,17 @@ btnRoll.addEventListener('click', function() {
     if (dice !== 1) {
         // Add dice to current score
         currentScore += dice;
+        // Selects score dynamically based on which is the current active player.
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
         // TODO: Change Later
-        current0El.textContent = currentScore; 
-
+        // current0El.textContent = currentScore; 
     } else {
         // switch to next player
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        currentScore = 0; 
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 
 })
